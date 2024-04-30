0dd506cd6eb616690bee89094fd335d00c8737ad
olivier <olivier.boesch@ac-aix-marseille.fr>
2024-04-30 13:27:49 +0200

    update ammos

--- no.py
+++ no.py
@@ -3,13 +3,18 @@
 import pyxel
 import random
 
-pyxel.init(128, 128)
+pyxel.init(128, 128, fps=30)
 pyxel.load("no.pyxres")
 
 vaisseau_x = 60
 vaisseau_y = 60
 
+TOUCHE_TIR = pyxel.KEY_SPACE
+GAMEPAD_TIR = pyxel.GAMEPAD1_BUTTON_B
+
 vies = 4
+
+munition = 10
 
 tirs_liste = []
 
@@ -17,8 +22,10 @@ ennemis_liste = []
 
 explosions_liste = []
 
+points = 0
 
 def vaisseau_deplacement(x, y):
+    global munition
     if pyxel.btn(pyxel.KEY_D) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_RIGHT):
         if x < 120:
             x = x + 1
@@ -31,6 +38,9 @@ def vaisseau_deplacement(x, y):
     if pyxel.btn(pyxel.KEY_Z) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_UP):
         if y > 0:
             y = y - 1
+    if pyxel.btn(pyxel.KEY_R) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_A):
+        munition = 10
+
     return x, y
 
 def vaisseau_suppression(vies):
@@ -42,17 +52,26 @@ def vaisseau_suppression(vies):
     return vies
             
 def ennemis_suppression():
+    global points
     for ennemi in ennemis_liste:
         for tir in tirs_liste:
-            if ennemi[0] <= tir [0]+1 and ennemi[0]+8 >= tir[0] and ennemi[1]+8 >= tir[1]:
+            x_tir = tir[0]
+            y_tir = tir[1]
+            x_ennemi = ennemi[0]
+            y_ennemi = ennemi[1]
+            if x_tir>x_ennemi-8 and x_tir< x_ennemi+8 and y_tir< y_ennemi+8:
                 ennemis_liste.remove(ennemi)
                 tirs_liste.remove(tir)
-                explosions_creation(ennemi[0], ennemi[1])
+                explosions_creation(x_ennemi, y_ennemi)
+                points += 1
 
 
 def tirs_creation(x, y, tirs_liste):
-    if pyxel.btnp(pyxel.KEY_SPACE, repeat=15) or pyxel.btnp(pyxel.GAMEPAD1_BUTTON_Y, repeat=15):
-       tirs_liste.append([x  , y-4])
+    global munition
+    if munition > 0:
+        if pyxel.btnp(TOUCHE_TIR, repeat=15) or pyxel.btnp(GAMEPAD_TIR, repeat=15):
+            tirs_liste.append([x, y-4])
+            munition -= 1
     return tirs_liste
 
 def tirs_deplacement(tirs_liste):
@@ -84,7 +103,10 @@ def explosions_animation():
             explosions_liste.remove(explosion)
 
 def update():
-    global vaisseau_x, vaisseau_y, tirs_liste, ennemis_liste ,vies, explosions_liste
+    global vaisseau_x, vaisseau_y, tirs_liste, ennemis_liste ,vies, explosions_liste, points
+
+    if pyxel.frame_count % 900 == 0:
+        points =points+10
 
     vaisseau_x, vaisseau_y = vaisseau_deplacement(vaisseau_x, vaisseau_y)
 
@@ -109,6 +131,14 @@ def draw():
     if vies > 0:
 
         pyxel.text(5, 5, 'VIES:'+ str(vies), 7)
+
+        pyxel.text(128-40, 5, 'POINTS:' + str(points), 7)
+
+        pyxel.blt(40,5, 0, 8, 0, 8, 8, 0)
+        pyxel.text(50, 5, str(munition), 7)
+
+        if munition == 0:
+            pyxel.text(vaisseau_x-10,vaisseau_y+10, 'RELOAD', pyxel.COLOR_RED)
 
         pyxel.blt(vaisseau_x, vaisseau_y, 0, 0,0 , 8, 8, 0)
 
