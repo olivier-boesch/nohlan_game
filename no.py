4f6402fe46c7ddd86a986cf1f2bae45f4c5a3947
olivier <olivier.boesch@ac-aix-marseille.fr>
2024-05-01 15:58:22 +0200

    added bg + music

--- no.py
+++ no.py
@@ -2,6 +2,7 @@
 
 import pyxel
 import random
+import json
 
 pyxel.init(128, 128, fps=30)
 pyxel.load("no.pyxres")
@@ -23,6 +24,36 @@ ennemis_liste = []
 explosions_liste = []
 
 points = 0
+
+vitesse_ennemi = 1
+
+etoiles = [[random.randint(0, 127), random.randint(0, 127), random.choice([7, 13, 9, 10, 5, 6])] for _ in range(80)]
+
+
+def play_music():
+    with open(f"music.json", "rt") as fin:
+        music = json.loads(fin.read())
+
+    for ch, sound in enumerate(music):
+        pyxel.sound(ch).set(*sound)
+        pyxel.play(ch, ch, loop=True)
+
+play_music()
+
+def deplacement_etoiles():
+    global etoiles
+    for i in range(len(etoiles)):
+        if etoiles[i][2] == 7 or etoiles[i][2] == 6 or etoiles[i][2] == 10:
+            etoiles[i][1] = etoiles[i][1] + 2
+        elif etoiles[i][2] == 13 or etoiles[i][2] == 9 or etoiles[i][2] == 5:
+            etoiles[i][1] = etoiles[i][1] + 1
+    n = 0
+    for e in etoiles:
+        if e[1] > 128:
+            etoiles.remove(e)
+            n += 1
+    for i in range (n):
+        etoiles.append([random.randint(0, 127), 0, random.choice([7, 13, 9, 10, 5, 6])])
 
 def vaisseau_deplacement(x, y):
     global munition
@@ -59,7 +90,7 @@ def ennemis_suppression():
             y_tir = tir[1]
             x_ennemi = ennemi[0]
             y_ennemi = ennemi[1]
-            if x_tir>x_ennemi-8 and x_tir< x_ennemi+8 and y_tir< y_ennemi+8:
+            if x_ennemi - 8 < x_tir < x_ennemi+8 and y_tir < y_ennemi+8:
                 ennemis_liste.remove(ennemi)
                 tirs_liste.remove(tir)
                 explosions_creation(x_ennemi, y_ennemi)
@@ -88,7 +119,7 @@ def ennemis_creation(ennemis_liste):
 
 def ennemis_deplacement(ennemis_liste):
     for ennemi in ennemis_liste:
-        ennemi[1] += 1
+        ennemi[1] += vitesse_ennemi
         if ennemi[1]>128:
            ennemis_liste.remove(ennemi)
     return ennemis_liste
@@ -103,10 +134,15 @@ def explosions_animation():
             explosions_liste.remove(explosion)
 
 def update():
-    global vaisseau_x, vaisseau_y, tirs_liste, ennemis_liste ,vies, explosions_liste, points
+    global vaisseau_x, vaisseau_y, tirs_liste, ennemis_liste ,vies, explosions_liste, points, vitesse_ennemi
+
+
 
     if pyxel.frame_count % 900 == 0:
         points =points+10
+        vitesse_ennemi = vitesse_ennemi+0.5
+
+    deplacement_etoiles()
 
     vaisseau_x, vaisseau_y = vaisseau_deplacement(vaisseau_x, vaisseau_y)
 
@@ -130,11 +166,17 @@ def draw():
 
     if vies > 0:
 
+        for e in etoiles:
+            x_etoile = e[0]
+            y_etoile = e[1]
+            col = e[2]
+            pyxel.rect(x_etoile, y_etoile, 1, 1, col)
+
         pyxel.text(5, 5, 'VIES:'+ str(vies), 7)
 
         pyxel.text(128-40, 5, 'POINTS:' + str(points), 7)
 
-        pyxel.blt(40,5, 0, 8, 0, 8, 8, 0)
+        pyxel.blt(40,2.5, 0, 8, 0, 8, 8, 0)
         pyxel.text(50, 5, str(munition), 7)
 
         if munition == 0:
@@ -152,6 +194,7 @@ def draw():
             pyxel.circb(explosion[0]+4, explosion[1]+4, 2*(explosion[2]//4), 8+explosion[2]%3)
     else:
         pyxel.text(50, 64, 'GAME OVER', 7)
+        pyxel.text(50, 74, 'SCORE :' +str(points) , 7)
 
 pyxel.run(update, draw)
 
